dc48ebad6767abec4a6d83699bc9dedb8dedfc48
Alex <alex6021710@gmail.com>
2020-05-08 14:33:04 +0300

    err add

--- errors/nft.go
+++ errors/nft.go
@@ -2,8 +2,10 @@ package errors
 
 import (
 	"errors"
+	"fmt"
 )
 
 var (
 	ErrInvalidL4ProtoType = errors.New("invalid protocol types parameter")
+	ErrTableWithNameNotFound = func(name string) error {return fmt.Errorf("nf table with name: %v, not found", name)}
 )
