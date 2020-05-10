fee7fe599c0b2842ee95dca5043379e21ce9273d
Alex <alex6021710@gmail.com>
2020-05-10 18:27:23 +0300

    add some enums and struct for fw

---
+++ consts/fw.go
@@ -0,0 +1,5 @@
+package consts
+
+const (
+	HexDigit = "0123456789abcdef"
+)
--- errors/nft.go
+++ errors/nft.go
@@ -9,4 +9,5 @@ var (
 	ErrInvalidL4ProtoType    = errors.New("invalid protocol types parameter")
 	ErrTableWithNameNotFound = func(name string) error { return fmt.Errorf("nf table with name: %v, not found", name) }
 	ErrInvalidActionType     = func(val string) error { return fmt.Errorf("invalid action type: %v", val) }
+	ErrInvalidIPVersionType = func(val string) error { return fmt.Errorf("invalid ip version type: %v", val) }
 )
