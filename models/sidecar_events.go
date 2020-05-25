74855d329deb4b4c661d2e8a4bf05686e5226514
Alex <alex6021710@gmail.com>
2020-05-25 12:49:46 +0300

    last

--- models/sidecar_events.go
+++ models/sidecar_events.go
@@ -6,9 +6,17 @@ import (
 	"github.com/alex60217101990/types/enums"
 )
 
+type SidecarEvent interface {
+	GetValue() interface{}
+}
+
 type MACBlacklistEvent struct {
 	MAC    string
 	Action enums.NftActionType
+}
+
+func (e MACBlacklistEvent) GetValue() interface{} {
+	return e
 }
 
 type IPBlacklistEvent struct {
@@ -17,14 +25,26 @@ type IPBlacklistEvent struct {
 	Action enums.NftActionType
 }
 
+func (e IPBlacklistEvent) GetValue() interface{} {
+	return e
+}
+
 type PortBlacklistEvent struct {
 	Action enums.NftActionType
 	Port   PortKey
 }
 
+func (e PortBlacklistEvent) GetValue() interface{} {
+	return e
+}
+
 type MACBanEvent struct {
 	MAC      string
 	Duration time.Duration
+}
+
+func (e MACBanEvent) GetValue() interface{} {
+	return e
 }
 
 type IPBanEvent struct {
@@ -33,7 +53,15 @@ type IPBanEvent struct {
 	Duration time.Duration
 }
 
+func (e IPBanEvent) GetValue() interface{} {
+	return e
+}
+
 type PortBanEvent struct {
 	Port     PortKey
 	Duration time.Duration
+}
+
+func (e PortBanEvent) GetValue() interface{} {
+	return e
 }
