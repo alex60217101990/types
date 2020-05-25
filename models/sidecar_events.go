440d925a3a295c0f0c37085d8fecd9770d8bf083
Alex <alex6021710@gmail.com>
2020-05-25 23:34:47 +0300

    add gob registers

--- models/sidecar_events.go
+++ models/sidecar_events.go
@@ -1,10 +1,22 @@
 package models
 
 import (
+	"encoding/gob"
 	"time"
 
 	"github.com/alex60217101990/types/enums"
 )
+
+func init() {
+	// This type must match exactly what youre going to be using,
+	// down to whether or not its a pointer
+	gob.Register(&MACBlacklistEvent{})
+	gob.Register(&IPBlacklistEvent{})
+	gob.Register(&PortBlacklistEvent{})
+	gob.Register(&MACBanEvent{})
+	gob.Register(&IPBanEvent{})
+	gob.Register(&PortBanEvent{})
+}
 
 type SidecarEvent interface {
 	GetValue() interface{}
